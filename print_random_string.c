9fdd1f944d839639953c6623a9c60d6d841c6757
“ElamTemplegit config --global user.email “tur71527@temple.edu <al3373@drexel.edu>
2025-08-27 13:42:39 -0400

    message

--- print_random_string.c
+++ print_random_string.c
@@ -14,7 +14,7 @@ char* rand_string_alloc(size_t size)
          rand_string(s, size); // to be developed by the student
      }
      return s;
-}
+
 
 
 
@@ -27,7 +27,7 @@ int main()
 	/* seed the randomizer */
 	srand( (unsigned)time(NULL) );
 
-	printf("Today's random word: ");
+	printf("Today's random word is: ");
     str = rand_string_alloc(size );
     printf("%s",str);
     free (str);
